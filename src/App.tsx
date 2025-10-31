2953e3894ff4c23ed816dadb2c4035cc02fcda9f
Nishant Singh <nraj14908@gmail.com>
2025-11-01 00:30:21 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-4xl'>hello</div>
+      hi there
+    </>
+  )
+}
+
+export default App
